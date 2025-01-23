a954cf431ce4ac043a78b0cea0bfc3fa35d888cb
yuki00yossi <yuki00yoshioka@gmail.com>
2025-01-24 00:36:49 +0900

    csrfトークンを返すAPIを追加

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/csrf', function () {
+    return csrf_token();
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
